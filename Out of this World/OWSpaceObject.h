6a97a47f9d5aed54a09bad08e13e3ec5d2fe0b2a
ilbumy <ilbumy@hotmail.com>
2013-10-30 19:11:57 -0400

    error

---
+++ Out of this World/OWSpaceObject.h
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.h
+//  Out of this World
+//
+//  Created by InTouch Administrator on 10/30/13.
+//  Copyright (c) 2013 Ilbum Kwak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OWSpaceObject : NSObject
+
+@end
---
+++ Out of this World/OWSpaceObject.m
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.m
+//  Out of this World
+//
+//  Created by InTouch Administrator on 10/30/13.
+//  Copyright (c) 2013 Ilbum Kwak. All rights reserved.
+//
+
+#import "OWSpaceObject.h"
+
+@implementation OWSpaceObject
+
+@end

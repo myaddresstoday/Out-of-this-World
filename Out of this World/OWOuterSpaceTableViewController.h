03cc6a4167918d7f21a7fd17cef1e60dc3d0805d
ilbumy <ilbumy@hotmail.com>
2013-10-29 21:54:57 -0400

    first commit

--- Out of this World/OWOuterSpaceTableViewController.h
+++ Out of this World/OWOuterSpaceTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  OWViewController.h
+//  OWOuterSpaceTableViewController.h
 //  Out of this World
 //
 //  Created by InTouch Administrator on 10/29/13.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface OWViewController : UIViewController
+@interface OWOuterSpaceTableViewController : UITableViewController
 
 @end
